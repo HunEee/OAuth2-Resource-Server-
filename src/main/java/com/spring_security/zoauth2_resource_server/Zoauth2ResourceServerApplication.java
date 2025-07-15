eaefe292e6ec8585f8e9e39f23fef21bd0c8e77c
HunEee <asdf981001@naver.com>
2025-07-15 22:18:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/spring_security/zoauth2_resource_server/Zoauth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security.zoauth2_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zoauth2ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zoauth2ResourceServerApplication.class, args);
+	}
+
+}
